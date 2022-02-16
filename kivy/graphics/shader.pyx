6a85a3d6babcbd6e1681914b44198dde60c9f67f
Sipondo <tt_robroek@hotmail.com>
2022-02-16 11:15:56 +0100

    supress build

--- kivy/graphics/shader.pyx
+++ kivy/graphics/shader.pyx
@@ -553,10 +553,10 @@ cdef class Shader:
         self._current_vertex_format = vertex_format
 
     cdef int build(self) except -1:
-        self.build_fragment(0)
-        self.build_geometry(0)
-        self.build_vertex(0)
-        self.link_program()
+        # self.build_fragment(0)
+        # self.build_geometry(0)
+        # self.build_vertex(0)
+        # self.link_program()
         return 0
 
     cdef int build_vertex(self, int link=1) except -1:
@@ -568,8 +568,8 @@ cdef class Shader:
         if self.vertex_shader is not None:
             cgl.glAttachShader(self.program, self.vertex_shader.shader)
             log_gl_error('Shader.build_vertex-glAttachShader')
-        # if link:
-        #     self.link_program()
+        if link:
+            self.link_program()
         return 0
     
     # NEW
@@ -585,8 +585,8 @@ cdef class Shader:
         if self.geometry_shader is not None:
             cgl.glAttachShader(self.program, self.geometry_shader.shader)
             log_gl_error('Shader.build_geometry-glAttachShader')
-        # if link:
-        #     self.link_program()
+        if link:
+            self.link_program()
         return 0
 
     cdef int build_fragment(self, int link=1) except -1:
